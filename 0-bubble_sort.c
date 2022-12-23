167916705b088eb05679f58171ba509c9dbe4170
uoch <chaouch.thameurtc61@gmail.com>
2022-12-23 15:23:46 +0100

    test

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,21 +6,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int action;
-    if (!array || !size)
-        return;
-    for (i = 0; i < size - 1; ++i)
-    {
-        for (j = 0; j < size - 1 - i; ++j)
-        {
-            if (array[j] > array[j + 1])
-            {
-                action = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = action;
-                print_array(array, size);
-            }
-        }
-    }
+size_t i, j;
+int action;
+
+if (!array || !size)
+return;
+for (i = 0; i < size - 1; ++i)
+{
+for (j = 0; j < size - 1 - i; ++j)
+{
+if (array[j] > array[j + 1])
+{
+action = array[j];
+array[j] = array[j + 1];
+array[j + 1] = action;
+print_array(array, size);
+}
+}
+}
 }
