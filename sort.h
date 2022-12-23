5e5a509a7d4d3ecee00c20570f39f539e7e4eb53
uoch <chaouch.thameurtc61@gmail.com>
2022-12-23 15:20:48 +0100

    test

--- sort.h
+++ sort.h
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
@@ -25,9 +25,5 @@ void insertion_sort_list(listint_t **list);
 int partition(int *array, int low, int high, size_t size);
 void quick(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
-
-
-
-
 
 #endif
